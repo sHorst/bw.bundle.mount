e1135950d038545de3f94057bb7bef2687cd18bf
Stefan Horst <stefan@ultrachaos.de>
2021-08-08 20:12:55 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # bw.bundle.mount
+
+## defaults
+```python
+metadata = {
+    'mount': {
+        'discs': [
+            # /dev/mapper/fry-root                            /                                               ext4    discard,noatime,errors=remount-ro       0       1
+            {
+                'file_system': '/dev/mapper/fry-root',
+                'mount_point': '/',
+                'type': 'ext4',
+                'options': ['discard', 'noatime', 'errors=remount-ro'],
+                'pass': 1,
+            },
+            # UUID=c4c7e595-4c96-4a43-9629-e4316a9da0fc       /boot                                           ext2    defaults                                0       2
+            {
+                'file_system': 'UUID=c4c7e595-4c96-4a43-9629-e4316a9da0fc',
+                'mount_point': '/boot',
+                'type': 'ext2',
+                'options': ['defaults'],
+                'pass': 2,
+            },
+        ],
+
+        'binds': [
+            {'from': '/mnt/test123', 'to': '/home/test123', },
+        ],
+
+        'swap': [
+            # /mnt/raid/swapfile1                             none                                            swap    sw                                      0       0
+            {'file_system': '/mnt/raid/swapfile1', 'options': ['sw']},
+        ],
+
+        'tmp': [
+            # tmpfs                                           /tmp                                            tmpfs   defaults,size=64g                       0       0
+            {'mount_point': '/tmp', 'size': '64g', },
+        ],
+
+        'network': [
+            # 192.168.2.123:/                                /mnt/raid                                        nfs     defaults                                0       0
+            {
+                'server': '192.168.2.123:/',
+                'mount_point': '/mnt/raid',
+                'type': 'nfs',
+                'options': ['defaults'],
+            },
+        ],
+    },
+}
+
+```
---
+++ metadata.py
@@ -0,0 +1,96 @@
+defaults = {}
+
+
+@metadata_reactor
+def add_nfs_apt(metadata):
+    if not node.has_bundle("apt"):
+        raise DoNotRunAgain
+
+    types = [x.get('type', None) for x in metadata.get('mount/network', [])]
+    packages = {}
+    if 'nfs' in types:
+        packages['nfs-common'] = {'installed': True, }
+
+    return {
+        'apt': {
+            'packages': packages,
+        }
+    }
+
+
+@metadata_reactor
+def convert_typed_mounts_to_mount_points(metadata):
+    # only manipulate metadata, if mount is set
+    if metadata.get('mount', None) is None:
+        return {}
+
+    mounts = []
+
+    mounts += [{'comment': 'discs'}]
+    mounts += metadata.get('mount/discs', [])
+
+    # add binds
+    if 'binds' in metadata.get('mount', {}):
+        mounts += [{'comment': ''}, {'comment': 'binds'}]
+        for bind in metadata.get('mount/binds', []):
+            mounts += [
+                {
+                    'file_system': bind['from'],
+                    'mount_point': bind['to'],
+                    'type': 'none',
+                    'options': ['bind'],
+                    'dump': 0,
+                    'pass': 0,
+                },
+            ]
+
+    # add swap
+    if 'swap' in metadata.get('mount', {}):
+        mounts += [{'comment': ''}, {'comment': 'swap'}]
+        for swap in metadata.get('mount/swap', []):
+            mounts += [
+                {
+                    'file_system': swap['file_system'],
+                    'mount_point': 'none',
+                    'type': 'swap',
+                    'options': swap.get('options', ['sw']),
+                    'dump': 0,
+                    'pass': 0,
+                },
+            ]
+
+    # add tmp
+    if 'tmp' in metadata.get('mount', {}):
+        mounts += [{'comment': ''}, {'comment': 'tmp'}]
+        for tmp in metadata.get('mount/tmp', []):
+            mounts += [
+                {
+                    'file_system': 'tmpfs',
+                    'mount_point': tmp['mount_point'],
+                    'type': 'tmpfs',
+                    'options': ['defaults', 'size=' + tmp.get('size', '1g'), ],
+                    'dump': 0,
+                    'pass': 0,
+                },
+            ]
+
+    # add network
+    if 'network' in metadata.get('mount', {}):
+        mounts += [{'comment': ''}, {'comment': 'network'}]
+        for network in metadata.get('mount/network', []):
+            mounts += [
+                {
+                    'file_system': network['server'],
+                    'mount_point': network['mount_point'],
+                    'type': network.get('type', 'nfs'),
+                    'options': network.get('options', ['defaults']),
+                    'dump': 0,
+                    'pass': 0,
+                },
+            ]
+
+    return {
+        'mount': {
+            'mounts': mounts,
+        },
+    }
